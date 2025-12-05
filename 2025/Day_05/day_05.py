58a5ef31047c37317b7c0e392fb6fd683d9b3fab
Danail Georgiev <danailgrgv@gmail.com>
2025-12-05 10:16:13 +0100

    Solved 2025 day 5

--- 2025/Day_05/day_05.py
+++ 2025/Day_05/day_05.py
@@ -1,89 +1,108 @@
-# Part 1
-
-VOWELS = "aeiou"
-BAD_STRINGS = ["ab", "cd", "pq", "xy"]
-
-# checks a string for having at least three vowels
-def check_for_three_vowels(string_input):
-    vowel_counter = 0
-
-    for letter in string_input:
-        if(letter in VOWELS):
-            vowel_counter += 1
-    
-    if(vowel_counter >= 3):
-        return True
-    return False
-
-# checks a string if it contains a letter that repeats consecutively
-def check_for_twice_in_a_row(string_input):
-    for i, letter in enumerate(string_input):
-        # end of string
-        if(i == len(string_input) - 1):
-            return False
-        if letter == string_input[i+1]:
-            return True
-    return False
-
-# checks if the string contains any of the "bad" strings
-def check_for_no_bad_strings(string_input):
-    for bad_string in BAD_STRINGS:
-        if(bad_string in string_input):
-            return False
-    return True
-
-# checks a string if it adheres to all three rules
-def is_nice_part_one(string_input):
-    return(check_for_three_vowels(string_input) and check_for_twice_in_a_row(string_input) and check_for_no_bad_strings(string_input))
-
 f = open("input.txt", "r")
-instructions = f.read()
-strings = instructions.splitlines()
+input = f.read()
+lines = input.splitlines()
 f.close()
 
-counter = 0
+ranges = []
+ingredients = []
+reading_ranges = True
 
-for string in strings:
-    if(is_nice_part_one(string)):
-        counter+=1
-
-print("nice strings (part 1) = ", counter)
-
-# Part 2
-
-# checks if a pair of letters repeats in the string
-def rule_one(string_input):
-    for index, letter in enumerate(string_input):
-        # check for end of string
-        if(index >= len(string_input) - 1):
-            return False
-        # take a pair of digits
-        pair = letter + string_input[index + 1]
-        # check the remainder of the string for a repetition of this pair
-        if(pair in string_input[index + 2:]):
-            return True
-
-# checks if at least one letter repeats with exactly 1 letter inbetween them
-def rule_two(string_input):
-    for index, letter in enumerate(string_input):
-        # checks for end of string
-        if(index >= len(string_input) - 2):
-            return False
-        # checks the letter 1 over if its the same
-        if(string_input[index + 2] == letter):
-            return True
-
-# checks the string if it adheres to both rules
-def is_nice_part_two(string_input):
-    if(rule_one(string_input) and rule_two(string_input)):
-        return True
+for line in lines:
+    if(line == ''):
+        reading_ranges = False
+        continue
+    if(reading_ranges):
+        ranges.append(line)
     else:
-        return False
-    
-counter = 0
+        ingredients.append(int(line))
 
-for string in strings:
-    if(is_nice_part_two(string)):
-        counter+=1
+print(ranges)
+print(ingredients)
 
-print("nice strings (part 2) = ", counter)
+# get range as a 2-length array
+def get_range(range_str):
+    range_arr = []
+    range_arr.append(int((range_str.split('-')[0])))
+    range_arr.append(int((range_str.split('-')[1])))
+    return range_arr
+
+# checks if an ingredient is in any of the ranges
+def is_fresh(ingredient, ranges):
+    for range_str in ranges:
+        range_arr = get_range(range_str)
+        if(ingredient >= range_arr[0]) and (ingredient <= range_arr[1]):
+            return True
+    return False
+
+# go through all the ingredients and count the fresh ones
+fresh_counter = 0
+for ingredient in ingredients:
+    if(is_fresh(ingredient, ranges)):
+        fresh_counter += 1
+
+print("Total number of fresh ingredients: ", fresh_counter)
+
+# check if 2 ranges are overlapping
+def are_overlapping(range_str_1, range_str_2):
+        range_arr_1 = get_range(range_str_1)
+        range_arr_2 = get_range(range_str_2)
+
+        # Check if first range starts before second
+        if(range_arr_1[0] <= range_arr_2[0]):
+            # Check if second range starts before first ends
+            if(range_arr_2[0] <= range_arr_1[1]):
+                return(True)
+        # Check if second range starts before first
+        if(range_arr_2[0] <= range_arr_1[0]):
+            # Check if first range starts before second ends
+            if(range_arr_1[0] <= range_arr_2[1]):
+                return(True)
+        return(False)
+
+# explicit, no?
+def get_number_of_valid_IDs_in_range(range_str):
+    if(range_str == ''):
+        return 0
+    range_arr = get_range(range_str)
+    return (range_arr[1] - range_arr[0] + 1)
+
+# finds a pair of overlapping ranges and joins them
+# returns True if it joined any ranges, 0 if none
+def find_and_join_ranges(ranges):
+    for i, range_str_1 in enumerate(ranges):
+        if(range_str_1 == ''):
+            continue
+        for j, range_str_2 in enumerate(ranges):
+            if(range_str_2 == ''):
+                continue
+            if(i == j):
+                continue
+            if(are_overlapping(range_str_1, range_str_2)):
+                range_arr_1 = get_range(range_str_1)
+                range_arr_2 = get_range(range_str_2)
+
+                if(range_arr_1[0] <= range_arr_2[0]):
+                    start = str(range_arr_1[0])
+                else:
+                    start = str(range_arr_2[0])
+
+                if(range_arr_1[1] <= range_arr_2[1]):
+                    end = str(range_arr_2[1])
+                else:
+                    end = str(range_arr_1[1])
+
+                new_range_str = start + '-' + end
+                ranges[i] = new_range_str
+                ranges[j] = ''
+                return True
+            
+    return False
+
+# keep going until all overlapping ranges are merged
+while(find_and_join_ranges(ranges)): pass
+
+fresh_ID_counter = 0
+for range in ranges:
+    fresh_ID_counter += get_number_of_valid_IDs_in_range(range)
+
+print("Total number of fresh IDs: ", fresh_ID_counter)
